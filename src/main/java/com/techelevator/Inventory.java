794adbe38f2b4f960def9676edbedbd764d28a62
Matthew Penman <penm4n@gmail.com>
2021-02-04 17:46:15 -0500

    lets try this again

--- src/main/java/com/techelevator/Inventory.java
+++ src/main/java/com/techelevator/Inventory.java
@@ -1,5 +1,18 @@
 package com.techelevator;
 
-public class Inventory {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Inventory  {
+	String path = "vendingmachine.csv";
+	
+	 File inputFile = new File(path);
+	 
+	 try Scanner fileInput = new Scanner(inputFile); 
+		 
+	 }catch(FileNotFoundException e) {
+		 
+	 }
+}
 }
