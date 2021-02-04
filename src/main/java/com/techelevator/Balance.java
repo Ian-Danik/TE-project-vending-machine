23bcad363a3183cc06be258fc7d6c4906bbfd18e
Matthew Penman <penm4n@gmail.com>
2021-02-04 15:42:17 -0500

    Added balance class

---
+++ src/main/java/com/techelevator/Balance.java
@@ -0,0 +1,65 @@
+package com.techelevator;
+
+public class Balance {
+	private double money;
+	private int quarters = 0;
+	private int dimes = 0;
+	private int nickels = 0;
+	private int pennies = 0;
+
+	public double getMoney() {
+		return money;
+	}
+
+	public int getQuarters() {
+		return quarters;
+	}
+
+	public int getDimes() {
+		return dimes;
+	}
+
+	public int getNickels() {
+		return nickels;
+	}
+
+	public int getPennies() {
+		return pennies;
+	}
+
+	public Balance(double money) {
+		this.money = money;
+	}
+
+	public double moneyIn(double moneyInput) {
+		money += moneyInput;
+		return money;
+	}
+
+	public void displayMoney() {
+		System.out.println("Current money is " + money);
+	}
+
+	public double moneyOut(double moneyInput) {
+		money -= moneyInput;
+		return money;
+	}
+
+	public void cashOut() {
+		while (money >= 25){
+			quarters = quarters++; 
+			money -= 25;
+			}
+		while (money >= 10){ 
+			quarters = dimes++;
+			money -= 10; 
+			}
+		while (money >= 5){
+			quarters = nickels++; 
+			money -= 10; 
+			}
+			pennies = (int) money;
+
+	}
+
+}
