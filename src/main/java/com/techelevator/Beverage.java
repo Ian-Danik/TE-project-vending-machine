82bb6cb953bcd069709b5a44ae06a9e1061f36d9
Matthew Penman <penm4n@gmail.com>
2021-02-04 15:02:29 -0500

    Lets try this again

--- src/main/java/com/techelevator/Beverage.java
+++ src/main/java/com/techelevator/Beverage.java
@@ -1,5 +1,21 @@
 package com.techelevator;
 
-public class Beverage {
-
+public class Beverage implements Purchasable {
+	private String type = "beverage";
+	private String message = "Glug Glug, Yum!";
+	private String name;
+	private double price;
+	
+	public String getName() {
+		return name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	private String getType() {
+		return type;
+	}
+	private String getMessage() {
+		return message;
+	}
 }
--- src/main/java/com/techelevator/Chips.java
+++ src/main/java/com/techelevator/Chips.java
@@ -1,5 +1,22 @@
 package com.techelevator;
 
-public class Chips {
+public class Chips implements Purchasable{
+	private String type = "chips";
+	private String message = "Crunch Crunch, Yum!";
+	private String name;
+	private double price;
+	
+	public String getName() {
+		return name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	private String getType() {
+		return type;
+	}
+	private String getMessage() {
+		return message;
+	}
 
 }
